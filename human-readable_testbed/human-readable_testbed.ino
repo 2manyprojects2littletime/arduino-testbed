b15af9b1055f0429f79da4414dee1a38ed23c1bf
Matt Duffy <m.duffy12610@gmail.com>
2015-01-24 13:19:10 -0400

    fixed compile errors

--- human-readable_testbed/human-readable_testbed.ino
+++ human-readable_testbed/human-readable_testbed.ino
@@ -1,13 +1,27 @@
-void func*[] tests;
+typedef int (*testFunction)();
+
+const int numberOfTests = 1;
+
+testFunction tests[numberOfTests];
+
+int smokeTest() {
+  return 0;
+}
 
 void setup () {
-  // init pins
-  
-  // register tests
-  
+  return;
 }
 
-void smokeTest() {
-  
+void registerTests() {
+  tests[0] = &smokeTest;
 }
 
+int main() {
+  static int testResults[numberOfTests];
+  setup();
+  registerTests();
+  for (int i = sizeof(tests); i > 0; i--) {
+    testResults[i] = tests[i]();
+  }
+  return testResults[0];
+}
